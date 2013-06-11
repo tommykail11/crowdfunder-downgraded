a4325adf99d38c0c238f46e880f2acddffbe84d2
Tommy Kail <tommykail@gmail.com>
2013-06-11 13:44:06 -0400

    Generating Project model with validations

---
+++ app/models/project.rb
@@ -0,0 +1,9 @@
+class Project < ActiveRecord::Base
+  belongs_to :user
+  attr_accessible :description, :goal, :teaser, :title
+
+  validates :title, presence: true, length: {maximum: 100}
+  validates :teaser, presence: true, length: {maximum: 255}
+  validates :description, presence: true
+  validates :goal, presence: true, numericality: {greater_than: 0, allow_blank: true}
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
   authenticates_with_sorcery!
-  # attr_accessible :title, :body
+  
+  has_many :projects
 end
